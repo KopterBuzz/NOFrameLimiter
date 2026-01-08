b2be87a4801e4e82c463261f85f18871fe499d93
Gabor Nemeth <etgabo@gmail.com>
2026-01-08 13:52:13 +0000

    initial upload

---
+++ NOFrameLimiter/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace NOFrameLimiter
+{
+    [BepInPlugin("KopterBuzz.NOFrameLimiter", "NOFrameLimiter", "0.0.1")]
+    [BepInProcess("NuclearOption.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource? Logger;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Configuration.InitSettings(Config);
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin NOFrameLimiter is loaded!");
+            Logger.LogInfo($"FrameRateLimit: {Configuration.FrameRateLimit.Value}");
+            Application.targetFrameRate = Configuration.FrameRateLimit.Value;
+            SetTargetFrameRate();
+        }
+
+        private void Update()
+        {
+            if (Application.targetFrameRate != Configuration.FrameRateLimit.Value)
+            {
+                SetTargetFrameRate();
+            }
+
+        }
+
+        private static void SetTargetFrameRate()
+        {
+            if (Configuration.FrameRateLimit.Value < 1)
+            {
+                Application.targetFrameRate = -1;
+            } else
+            {
+                Application.targetFrameRate = Configuration.FrameRateLimit.Value;
+            }
+            Logger.LogInfo($"FrameRateLimit: {Application.targetFrameRate}");
+        }
+    }
+
+    public static class Configuration
+    {
+        internal const string GeneralSettings = "General Settings";
+        internal const int DefaultFrameRateLimit = 60;
+        internal static ConfigEntry<int> FrameRateLimit;
+        internal static void InitSettings(ConfigFile config)
+        {
+            Plugin.Logger?.LogInfo("Loading Settings.");
+            FrameRateLimit = config.Bind(GeneralSettings, "FrameRateLimit", DefaultFrameRateLimit, $"Maximum Desired Frame Rate. 0 = Unlimited. Default = {DefaultFrameRateLimit}");
+        }
+    }
+}
+
+
